cdf8d4e860b7e4392d5591c5cf5d99d83a78b3b4
Abdul Rafay Modi <rafaymodi18@gmail.com>
2021-01-29 10:58:44 +0500

    Form collapse is now working

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    include 'Crud.php';
-    $obj = new Crud();
+include 'Crud.php';
+$obj = new Crud();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,37 +19,38 @@
 <body>
     <!-- Header -->
     <div class="container box">
-            <h3 class="text-center">AJAX CRUD Operation Using OOP - Fetch Data</h3><br><br>
-        </div>
+        <h3 class="text-center">AJAX CRUD Operation Using OOP - Fetch Data</h3><br><br>
+    </div>
     <!-- MAIN -->
     <main>
         <div class="container">
+            <div class="render">&nbsp;</div>
             <div class="card-header">
                 <button type="button" name="add" class="btn btn-outline-primary" data-toggle="collapse" data-target="#user_collapse"><i class="fas fa-plus"></i></button>
                 <br><br>
                 <div id="user_collapse" class="collpase">
                     <form id="user_form" method="POST" class="form">
-                    <div class="form-group">
-                    <label for="first_name">First Name</label>
-                      <input type="text" name="first_name" id="first_name" class="form-control" placeholder="Eg. Muhammad Ali">
-                      <small class="text-muted">Enter your first name here</small>
-                    </div>
-                    <br>
-                    <div class="form-group">
-                    <label for="last_name">Last Name</label>
-                      <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Eg. Modi">
-                      <small class="text-muted">Enter your last name here</small>
-                    </div>
-                    <br>
-                    <div class="form-group">
-                    <label for="user_image">Image</label>
-                      <input type="file" name="user_image" id="user_image" class="form-control" placeholder="Eg. Modi">
-                      <small class="text-muted">Select your image</small>
-                    </div>
-                    <div align="center">
-                        <input type="hidden" name="action" id="action">
-                        <input type="submit" name="button_action" id="button_action" value="Insert" class="btn btn-outline-danger" />
-                    </div>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" name="first_name" id="first_name" class="form-control" placeholder="Eg. Muhammad Ali">
+                            <small class="text-muted">Enter your first name here</small>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Eg. Modi">
+                            <small class="text-muted">Enter your last name here</small>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="user_image">Image</label>
+                            <input type="file" name="user_image" id="user_image" class="form-control" placeholder="Eg. Modi">
+                            <small class="text-muted">Select your image</small>
+                        </div>
+                        <div align="center">
+                            <input type="hidden" name="action" id="action">
+                            <input type="submit" name="button_action" id="button_action" value="Insert" class="btn btn-outline-danger" />
+                        </div>
                     </form>
                 </div>
             </div>
@@ -59,42 +60,37 @@
         <div id="user_table" class="table-responsive">
         </div>
     </main>
-    <!-- Bootstrap 4 Java Script -->
-    <script src="assets/scripts/bootstrap.min.js"></script>
-    <!-- jQuery Scripts -->
     <script src="assets/scripts/jquery-3.5.1.min.js"></script>
-    <!-- Sweet Alert 2 CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
             load_data();
             $('#action').val("Insert");
-            function load_data()
-            {
+
+            function load_data() {
                 var action = "Load";
                 $.ajax({
                     url: "action.php",
                     method: "POST",
-                    data:{action:action},
-                    success: function(data)
-                    {
+                    data: {
+                        action: action
+                    },
+                    success: function(data) {
                         $('#user_table').html(data);
                     }
                 });
             }
 
-            $('#user_form').on('submit' ,function(event)
-            {
+            $('#user_form').on('submit', function(event) {
                 event.preventDefault();
-                var firstName   = $('#first_name').val();
-                var lastName    = $('#last_name').val();
-                var extension   = $('#user_image').val().split('.').pop().toLowerCase();
-                if(extension != '') 
-                {
-                    if(jQuery.inArray(extension, ['gif', 'png', 'jpg', 'jpeg']) == -1)
-                    {
-                        Swal.fire(
-                        {
+                var firstName = $('#first_name').val();
+                var lastName = $('#last_name').val();
+                var extension = $('#user_image').val().split('.').pop().toLowerCase();
+                if (extension != '') {
+                    if (jQuery.inArray(extension, ['gif', 'png', 'jpg', 'jpeg']) == -1) {
+                        Swal.fire({
                             title: 'Error!',
                             text: 'Invalid Image File',
                             icon: 'error',
@@ -104,40 +100,35 @@
                         return false;
                     }
                 }
-                if(firstName != '' || lastName != '')
-                {
+                if (firstName != '' || lastName != '') {
                     $.ajax({
                         url: 'action.php',
                         method: 'POST',
-                        data:new FormData(this),
-                        contentType:false,
-                        processData:false,
-                        success:function(data)
-                        {
-                            Swal.fire(
-                        {
-                            title: 'Success',
-                            text: data,
-                            icon: 'success',
-                            confirmButtonText: 'Cool'
-                        });
-                        $('#user_form')[0].reset();
-                        load_data();
+                        data: new FormData(this),
+                        contentType: false,
+                        processData: false,
+                        success: function(data) {
+                            Swal.fire({
+                                title: 'Success',
+                                text: data,
+                                icon: 'success',
+                                confirmButtonText: 'Cool'
+                            });
+                            $('#user_form')[0].reset();
+                            load_data();
                         }
                     })
-                }
-                else 
-                {
-                    Swal.fire(
-                        {
-                            title: 'Error!',
-                            text: 'Both fields are required',
-                            icon: 'error',
-                            confirmButtonText: 'Cool'
-                        });
+                } else {
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'Both fields are required',
+                        icon: 'error',
+                        confirmButtonText: 'Cool'
+                    });
                 }
             });
         });
     </script>
 </body>
+
 </html>
