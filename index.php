fad190edb691aa2c71629305d0c42e92b09bc492
Abdul Rafay Modi <subjectalpha32@gmail.com>
2021-01-29 14:29:56 +0500

    Form fix now working

--- index.php
+++ index.php
@@ -59,11 +59,9 @@
         <div id="user_table" class="table-responsive">
         </div>
     </main>
-    <!-- Bootstrap 4 Java Script -->
-    <script src="assets/scripts/bootstrap.min.js"></script>
-    <!-- jQuery Scripts -->
     <script src="assets/scripts/jquery-3.5.1.min.js"></script>
-    <!-- Sweet Alert 2 CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script>
         $(document).ready(function(){
